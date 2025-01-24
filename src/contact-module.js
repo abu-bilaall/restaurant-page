d3d952e299319215492f3dfe7146f6d4b5808a53
Abdulrofihi Musa <m.abdulrofihi@gmail.com>
2025-01-24 09:19:46 +0100

    Add contact module and contact styles

--- src/contact-module.js
+++ src/contact-module.js
@@ -1,4 +1,7 @@
 import ("./contact.css");
+import locationSVG from "../assets/images/address.svg";
+import phoneSVG from "../assets/images/phone.svg";
+import map from "../assets/images/map-snapshot.png";
 
 export function loadContactPage() {
     // clear contents
@@ -7,5 +10,43 @@ export function loadContactPage() {
         content.removeChild(content.firstChild);
     }
 
+    const body = document.querySelector('body');
+    body.setAttribute('id', 'blur-bg');
 
+    const contactCard = document.createElement('div');
+    contactCard.setAttribute('class', 'contact-card');
+
+    // contact phone
+    const contactPhone = document.createElement('div');
+    contactPhone.setAttribute('class', 'contact-phone');
+
+    const phoneIcon = document.createElement('img');
+    phoneIcon.setAttribute('src', phoneSVG);
+
+    const phoneDigits = document.createElement('div');
+    phoneDigits.textContent = "Phone: +1 (555) 123-4567";
+
+    // contactPhone.append(phoneIcon, phoneDigits);
+    contactPhone.appendChild(phoneDigits);
+
+    // contact address
+    const contactAddress = document.createElement('div');
+    contactAddress.setAttribute('class', 'contact-addy');
+
+    const addyIcon = document.createElement('img');
+    phoneIcon.setAttribute('src', locationSVG);
+
+    const addy = document.createElement('div');
+    addy.textContent = "Address: 123 Main Street, Springfield, USA";
+
+    // contactAddress.append(addyIcon, addy);
+    contactAddress.appendChild(addy)
+
+    // map image
+    const mapImg = document.createElement('img');
+    mapImg.setAttribute('src', map);
+
+    contactCard.append(contactPhone, contactAddress, mapImg);
+
+    content.appendChild(contactCard);
 }
