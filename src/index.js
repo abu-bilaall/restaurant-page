249ffc32080bb55585a3805249fd87b6cb675ddb
Abdulrofihi Musa <m.abdulrofihi@gmail.com>
2025-01-24 09:37:20 +0100

    Import module menu and configure menu event

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import("./styles.css");
 import { loadHomePage } from "./initial-page-load";
 import { loadContactPage } from "./contact-module";
+import { loadMenuPage } from "./menu-module";
 
 loadHomePage();
 
 const home = document.querySelector ('#home');
 home.addEventListener('click', () => {
     loadHomePage();
+})
+
+const menu = document.querySelector('#menu');
+menu.addEventListener('click', () => {
+    loadMenuPage();
 })
 
 const contact = document.querySelector('#contact');
