700f9b30f39673f7c6fc0dc362a3debad0101a8b
Abdulrofihi Musa <m.abdulrofihi@gmail.com>
2025-01-27 19:33:05 +0100

    Install webpack-merge and split config files

--- webpack.common.js
+++ webpack.common.js
@@ -3,16 +3,11 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
     entry: "./src/index.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
         clean: true,
-    },
-    devtool: "eval-source-map",
-    devServer: {
-        watchFiles: ["./src/template.html"],
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -34,5 +29,5 @@ module.exports = {
                 type: "asset/resource",
             },
         ]
-    }
+    },
 }
